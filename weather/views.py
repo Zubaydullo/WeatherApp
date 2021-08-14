86ceb884738fd9da8b972aacf28b7869604ff91a
Zubaydullo <zubaycoder0022@gmail.com>
2021-08-14 19:54:55 +0500

    commented [main]

--- weather/views.py
+++ weather/views.py
@@ -17,11 +17,11 @@ def home(request):
 
 	for city in cities:
 		r = requests.get(url.format(city)).json()
-		# print(r.text)
+		print(r.text)
 
 		city_weather = {
 			'city': city.name,
-			# 'temperature': r['main']['temp'],
+			'temperature': r['main']['temp'],
 			'description': r['weather'][0]['description'],
 			'icon': r['weather'][0]['icon'],
 		}
